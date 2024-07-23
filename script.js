caad237040f7be218dd849730f1bd7d9ed26511a
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2024-07-23 14:27:40 +0100

    Added Dynamic To-do list

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       const removeButton = document.createElement("button");
       removeButton.textContent = "Remove";
-      removeButton.className = "remove-btn";
+      removeButton.classList.add("remove-btn");
       list.appendChild(removeButton);
 
       removeButton.addEventListener("click", function () {
